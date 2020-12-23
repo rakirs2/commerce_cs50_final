13e7733d0364423b91a72d276e96ff9390529fad
Srikar Mylavarapu <vsn611@gmail.com>
2020-12-22 18:26:51 -0600

    added auction listing model; first major change

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,9 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class AuctionListing(models.Model):
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name="poster");
+    title = models.CharField(max_length=50, blank=False, unique=True);
+    id = models.AutoField(primary_key=True);
